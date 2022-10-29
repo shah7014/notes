4a808f1489f05e22e4cd496f67f5cc1aedc683b0
Romil Shah <sromil@ford.com>
2022-10-29 10:54:22 +0530

    style: adding error style

--- src/components/NewGoalForm.js
+++ src/components/NewGoalForm.js
@@ -3,13 +3,21 @@ import "./NewGoalForm.css";
 
 const NewGoalForm = (props) => {
   const [newGoal, setNewGoal] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalChangeHandler = (event) => {
+    if (event.target.value.trim().length !== 0) {
+      setIsValid(true);
+    }
     setNewGoal(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (newGoal.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onSaveGoal({ title: newGoal, isDone: false });
     setNewGoal("");
   };
@@ -17,11 +25,14 @@ const NewGoalForm = (props) => {
   return (
     <div className="goal--form">
       <form onSubmit={handleSubmit}>
-        <label className="goal--form--label" htmlFor="title">
+        <label
+          className={`goal--form--label ${isValid ? "" : "invalid"}`}
+          htmlFor="title"
+        >
           Course Goal
         </label>
         <input
-          className="goal--form--input"
+          className={`goal--form--input ${isValid ? "" : "invalid"}`}
           id="title"
           value={newGoal}
           onChange={goalChangeHandler}
